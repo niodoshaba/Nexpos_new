76ca267fbd191d567722531e6cb69b6628c3c78c
Addie-Pi <cureantidote@hotmail.com>
2020-07-24 15:23:11 +0800

    幫康哥修改資料庫設定

--- dev/js/insertCustomerData.php
+++ dev/js/insertCustomerData.php
@@ -1,12 +1,7 @@
 <?php
 
 try{
-
-    $dsn = "mysql:host=localhost;post=3306;dbname=g4_nexpos;charset=utf8";
-    $user = "root";
-    $password = "1u3ru894jo4SPUR";
-    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-    $pdo = new PDO($dsn, $user, $password, $options);
+    require_once("generalConnectDB.php");
 
     $Data = $_POST["customerData"];
 
--- dev/js/tabSpecSaveToDB.php
+++ dev/js/tabSpecSaveToDB.php
@@ -1,18 +1,9 @@
 <?php
 
 try{
-    // $dsn = "mysql:host=localhost;post=3306;dbname=g4_nexpos;charset=utf8";
-    // $user = "root";
-    // $password = "1u3ru894jo4SPUR";
-    // $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-    // $pdo = new PDO($dsn, $user, $password, $options);
-
-    require_once("ordCon.php");
-
-
-    // require_once("generalConnectDB.php");
-    // require_once("ordCon.php");
-
+   
+    require_once("generalConnectDB.php");
+   
     // $pdo_1 = new PDO($dsn, $user, $password, $options);
 
 
